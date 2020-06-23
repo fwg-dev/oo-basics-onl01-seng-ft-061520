15c020d4e40646bcf30f4244f75bea1ca6aa200e
Faith  Gicheha <fwgicheh@gmail.com>
2020-06-23 19:05:13 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -4,29 +4,8 @@ class Shoe
   def initialize(brand)
     @brand = brand
   end
-#   def brand
-#     @brand
-#   end
-#   def color=(color)
-#     @color - color
-#   end
-#   def color
-#     @color
-#   end
-#   def size = (size)
-#     @size = size
-#   end
-#   def size
-#     @size
-#   end
-# def material=(material)
-#   @material= material
-# end
-# def condition=(condition)
-#   @condition = condition
 def cobble
   @condition = "new"
   puts "your shoe is as good as new"
 end
-# end
 end
