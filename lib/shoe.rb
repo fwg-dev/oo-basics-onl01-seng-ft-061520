d4e6f0b1ebfcba668d81e575f0313b1e4d81df1e
Faith  Gicheha <fwgicheh@gmail.com>
2020-06-23 18:43:52 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,9 @@
 # Make your shoe class here!
 class Shoe
   def initialize(brand)
-    @brand = brand 
+    @brand = brand
   end
-  def brand 
-    @brand 
-  end 
+  def brand
+    @brand
+  end
 end
