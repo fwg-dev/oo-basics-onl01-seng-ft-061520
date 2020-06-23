4d6904cdbf2a0458c162db1809bd80de6ec1a70a
Faith  Gicheha <fwgicheh@gmail.com>
2020-06-23 18:48:51 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,23 @@
 # Make your shoe class here!
 class Shoe
+  attr_accessor
   def initialize(brand)
     @brand = brand
   end
   def brand
     @brand
   end
+  def color=(color)
+    @color - color 
+  end 
+  def color
+    @color 
+  end 
+  def size = (size)
+    @size = size 
+  end 
+  def size 
+    @size 
+  end 
+      
 end
