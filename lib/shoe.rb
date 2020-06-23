aeab92a587187395cc1393afa291cb26979594eb
Faith  Gicheha <fwgicheh@gmail.com>
2020-06-23 19:03:29 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -24,9 +24,9 @@ class Shoe
 # end
 # def condition=(condition)
 #   @condition = condition
-def cobble 
-  @condition 
+def cobble
+  @condition
   puts "your shoe is as good as new"
-end 
+end
 # end
 end
