46968215a528e964f00c97a6cf086f7355724903
Faith  Gicheha <fwgicheh@gmail.com>
2020-06-23 18:05:46 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -17,7 +17,19 @@ class Book #("And Then There Were None")
   def page_count(num)
    @page_count = num
  end
- def page_count
-   @page_count
-end
+   def page_count
+     @page_count
+  end
+
+  def genre=(genre)
+    @genre = genre
+  end
+
+  def genre
+    @genre
+  end
+  
+  def turn_page
+  end
+ 
 end
